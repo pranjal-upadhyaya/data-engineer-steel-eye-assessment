88da4aeec27ccf9119147fc99f70324260e121a1
pranjal <rktpranjal@gmail.com>
2026-04-26 23:39:33 +0530

    intrdocuced batch processing, incremental data dump and removed adhoc limit introduced for testing

--- app/xml_parser.py
+++ app/xml_parser.py
@@ -18,12 +18,9 @@ class XMLParser:
         data = self.extract_xml_data(context)
 
         del context
-
-        self.dump_xml_data_to_csv(data)
     
 
     def extract_xml_data(self, context: Any):
-        loop = 100
         data = []
         for event, elem in context:
             if event == "start":
@@ -34,29 +31,42 @@ class XMLParser:
                 namespace = elem.tag.split("}")[0]+"}"
                 print(namespace)
                 fin = elem.find(f"{namespace}FinInstrmGnlAttrbts")
-                tag_list = ["Id", "FullNm", "ClssfctnTp", "CmmdtyDerivInd", "NtnlCcy", "Issr"]
+                tag_list = ["Id", "FullNm", "ClssfctnTp", "CmmdtyDerivInd", "NtnlCcy"]
                 for tag in tag_list:
                     val = self.extract_data_from_xml_ele(fin, namespace, tag)
                     data_set[f"FinInstrmGnlAttrbts.{tag}"] = val
+                val = self.extract_data_from_xml_ele(elem, namespace, "Issr")
+                data_set[f"Issr"] = val
                 print(data_set)
                 data.append(data_set)
-                # if len(data) >= self.batch_size:
-                #     self.dump_xml_data_to_csv(data)
-                #     data = []
+                if len(data) >= self.batch_size:
+                    self.dump_xml_data_to_csv(data)
+                    data = []
                 elem.clear()
                 while elem.getprevious() is not None:
                     del elem.getparent()[0]
                 print("========================")
-            
-            loop -= 1
-            if loop < 0:
-                break
+
+        if len(data) > 0:
+            self.dump_xml_data_to_csv(data)
+            data = []
         return data
 
     def dump_xml_data_to_csv(self, data: List[dict]):
         df = pd.DataFrame(data)
-                
-        df.to_csv("xml_data_dump.csv")
+
+        if self.first_write:
+            with open("xml_data_dump.csv", "w", newline="", encoding="utf-8") as file:
+                writer = csv.DictWriter(file, fieldnames=["FinInstrmGnlAttrbts.Id", "FinInstrmGnlAttrbts.FullNm", "FinInstrmGnlAttrbts.ClssfctnTp", "FinInstrmGnlAttrbts.CmmdtyDerivInd", "FinInstrmGnlAttrbts.NtnlCcy", "Issr"])
+                writer.writeheader()
+                writer.writerows(data)
+            self.first_write = False
+        else:
+            with open("xml_data_dump.csv", "a", newline="", encoding="utf-8") as file:
+                writer = csv.DictWriter(file, fieldnames=["FinInstrmGnlAttrbts.Id", "FinInstrmGnlAttrbts.FullNm", "FinInstrmGnlAttrbts.ClssfctnTp", "FinInstrmGnlAttrbts.CmmdtyDerivInd", "FinInstrmGnlAttrbts.NtnlCcy", "Issr"])
+                writer.writerows(data)
+
+        return None
 
     def extract_data_from_xml_ele(self, parent_element: Any, namespace: str, tag: str):
         element = parent_element.find(f"{namespace}{tag}")
